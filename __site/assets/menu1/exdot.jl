823e339a11f67315e7d2d491435c6057c19dd106
aadesh <aadeshdeshmukh25@gmail.com>
2020-06-23 16:01:26 +0530

    initial files

--- __site/assets/menu1/exdot.jl
+++
@@ -1,2 +0,0 @@
-# This file was generated, do not modify it. # hide
-TSdataset(""/Users/abc/MLJtime_pdf_data/GunPoint")
--- __site/assets/menu2/univariat.jl
+++
@@ -1,9 +0,0 @@
-# This file was generated, do not modify it. # hide
-using MLJTime
-train, test = load_gunpoint()
-X_train, y_train = X_y_split(train)
-X_test, y_test = X_y_split(test)
-forest = RandomForestClassifierTS(X_train, y_train)
-@show y_predict = predict_new(X_test, forest)
-@show y_test
-@show L1(y_predict, y_test)
